e1ebe1b875f346e6f25d9b93f3900afa72532bea
GoodsandObjects <kenny@goodsandobjects.com>
2026-01-22 13:09:41 -0500

    Initial Commit

---
+++ Seek/SeekApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeekApp.swift
+//  Seek
+//
+//  Created by Kenny Lloyd on 1/22/26.
+//
+
+import SwiftUI
+
+@main
+struct SeekApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
